003981cc46b33c421fb75801ec039b967fc823c7
jasaga2401 <jasaga@protonmail.com>
2023-11-20 13:31:32 +0000

    Add project files.

---
+++ c#_for_loop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        // Use a for loop to print numbers from 1 to 5
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"Current number: {i}");
+        }
+    }
+}
+
